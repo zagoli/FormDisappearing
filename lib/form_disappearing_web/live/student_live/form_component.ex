728e1dc499eb33e68e8a72cff9d31d3480fcafcc
Jacopo Zagoli <jacopo@zagoli.com>
2024-11-28 14:36:35 +0100

    the solution

--- lib/form_disappearing_web/live/student_live/form_component.ex
+++ lib/form_disappearing_web/live/student_live/form_component.ex
@@ -9,5 +9,7 @@ defmodule FormDisappearingWeb.StudentLive.Form do
   defp assign_form(socket) do
     changeset = Students.change_student(socket.assigns.student)
     socket |> assign(:form, to_form(changeset))
+    # The following line solves the issue (https://elixirforum.com/t/form-in-live-components-disappear-after-socket-update/67642/6)
+    # socket |> assign(:form, to_form(changeset, id: "student_#{socket.assigns.student.id}"))
   end
 end
